c2fcb82130328c33ad4ff325e916b74e0d4009b7
Sambert <sambertr@gmail.com>
2019-07-26 16:39:39 +0530

    change in country

--- src/context.js
+++ src/context.js
@@ -11,7 +11,7 @@ export class Provider extends Component {
   componentDidMount() {
     axios
       .get(
-        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=100&country=in&f_has_lyrics=1&apikey=${
           process.env.REACT_APP_MM_KEY
         }`
       )
