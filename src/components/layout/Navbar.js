faf501c9788c5206a9258b0dc11ea6b76de6e33e
Sambert <sambertr@gmail.com>
2019-07-26 16:23:40 +0530

    display top 10

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-default bg-dark mb-5">
+      <span className="nabar-brand mb-0 h1 mx-auto">LyricFinder</span>
+    </nav>
+  );
+};
+
+export default Navbar;
